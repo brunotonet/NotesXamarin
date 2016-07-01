7097d566455283d95cf01701aff60ee6d1d50e10
Weliton <Weliton@VM-DEV>
2016-07-01 16:56:26 -0300

    Add project files.

---
+++ NotesXamarin/NotesXamarin/App.cs
@@ -0,0 +1,55 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace NotesXamarin
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            MainPage = new Lista();
+        }
+
+        protected override void OnStart()
+        {
+            var db = new SQLiteConnection(DependencyService.Get<IDadosEspecificos>().CaminhoDB("NOTAS.DB"));
+            db.CreateTable<Nota>();
+            //db.Insert(new Nota() { Titulo = "xxx1", Descricao = "YYY1" });
+            //db.Insert(new Nota() { Titulo = "xxx2", Descricao = "YYY2" });
+            //db.Insert(new Nota() { Titulo = "xxx3", Descricao = "YYY3" });
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+
+    public interface IDadosEspecificos
+    {
+        String CaminhoDB(String NomeDB);
+    }
+
+    public class Nota
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string Descricao { get; set; }
+    }
+}
---
+++ NotesXamarin/NotesXamarin/Lista.xaml.cs
@@ -0,0 +1,29 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace NotesXamarin
+{
+    public partial class Lista : ContentPage
+    {
+        public Lista()
+        {
+            InitializeComponent();
+
+            var db = new SQLiteConnection(DependencyService.Get<IDadosEspecificos>().CaminhoDB("NOTAS.DB"));
+            ListaNotas.ItemsSource = db.Table<Nota>().ToList();
+
+            ListaNotas.ItemTapped += ListaNotas_ItemTapped;
+        }
+
+        private void ListaNotas_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            int ID = ((Nota)e.Item).Id;
+        }
+    }
+}
